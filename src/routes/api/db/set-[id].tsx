2a5e9d1fc71540fb9955a1714b5e18e341f14393
jjreset <jreset@naver.com>
2024-06-29 23:39:23 +0900

    fix

--- src/routes/api/db/set-[id].tsx
+++ src/routes/api/db/set-[id].tsx
@@ -1,7 +1,7 @@
-import store from "./store";
+// import store from "./store";
 
-export async function GET({ params }) {
-  console.log(`Category: ${params.category}, Brand: ${params.brand}`);
-  const products = await store.getProducts(params.category, params.brand);
-  return products;
-}
+// export async function GET({ params }) {
+//   console.log(`Category: ${params.category}, Brand: ${params.brand}`);
+//   const products = await store.getProducts(params.category, params.brand);
+//   return products;
+// }
--- src/routes/api/session.tsx
+++ src/routes/api/session.tsx
@@ -1,14 +1,14 @@
-import { getCookie } from "vinxi/http";
-import store from "./store";
+// import { getCookie } from "vinxi/http";
+// import store from "./store";
 
-export async function GET(event) {
-  const userId = getCookie("userId");
-  if (!userId) {
-    return new Response("Not logged in", { status: 401 });
-  }
-  const user = await store.getUser(event.params.userId);
-  if (user.id !== userId) {
-    return new Response("Not authorized", { status: 403 });
-  }
-  return user;
-}
+// export async function GET(event) {
+//   const userId = getCookie("userId");
+//   if (!userId) {
+//     return new Response("Not logged in", { status: 401 });
+//   }
+//   const user = await store.getUser(event.params.userId);
+//   if (user.id !== userId) {
+//     return new Response("Not authorized", { status: 403 });
+//   }
+//   return user;
+// }
