31ecd0d2491fa57b162332c5994b77e417ff7c2f
jjreset <jreset@naver.com>
2024-07-11 17:42:04 +0900

    test kakao

--- src/components/SetSend.tsx
+++ src/components/SetSend.tsx
@@ -11,9 +11,22 @@ type SetSendProps<P = {}> = P & {
   link: string;
 };
 
+// instagram://sharesheet?text={AnyTextOrLinkToShare}
+// https://twitter.com/share?text={TEXT}&url={URL}
+
 export default function SetSend(props: SetSendProps): JSX.Element {
   return (
     <>
+      <div>
+        <div>{props.link}</div>
+        <div>CopyButton</div>
+      </div>
+      <div>
+        <div>instagram</div>
+        <a href='kakaolink://send?appkey=47709efeaae6b8a5a3a04ba9966d08c0&appver=1.0&linkver=4.0&extras={"KA":"os/javascript lang/en-US","lcba":"serverCallbackArgs_if_any"}&request_url=https://party-cal.vercel.app'>kakaotalk</a>
+        <div>x</div>
+        <div>more</div>
+      </div>
     </>
   );
 }
