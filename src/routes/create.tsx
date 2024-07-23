f6b4a15e8c3eb3d792bb1101ddbdaf38b50e2d0d
jjreset <jreset@naver.com>
2024-07-23 14:53:35 +0900

    test

--- src/routes/create.tsx
+++ src/routes/create.tsx
@@ -12,42 +12,26 @@ type CreateStateProps = null | {
 
 export default function CreatePage() {
   const location = useLocation();
-  const state = createMemo<CreateStateProps>(() => location.state || {});
   const fetchLink = async (source: CreateStateProps) => {
     try {
-      if (!source.start || !source.end || !source.title) throw new Error();
-      const res = await fetch("https://api.partycal.site/create", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(source),
-      });
-      if (res.ok) return res.json();
-      else throw new Error();
+      if (source.start && source.end && source.title){
+        const res = await fetch("https://api.partycal.site/create", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(source),
+        });
+        if (res.ok) return res.json();
+        else throw new Error();
+      } else throw new Error();
     } catch {
       return null;
     }
   };
 
-  const [uid] = createResource(()=>state(), fetchLink);
+  const [uid] = createResource(()=>location.state || {}, fetchLink);
 
-  // const [uid] = createResource(async() => {
-  //   try {
-  //     if (!state()) throw new Error();
-  //     const res = await fetch("https://api.partycal.site/create", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(state()),
-  //     });
-  //     if (res.ok) return Promise.resolve(res.json());
-  //     else throw new Error();
-  //   } catch {
-  //     return Promise.resolve(null);
-  //   }
-  // });
 
   return (
     <SetBox>
