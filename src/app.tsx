31cb5f514c8f2a1d10fa4754139c80dfc16d9750
jjreset <jreset@naver.com>
2024-08-16 13:36:54 +0900

    reallt fix font?

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,10 @@
-import { MetaProvider, Title } from "@solidjs/meta";
+import { MetaProvider, Title, Link } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import type { RouteSectionProps } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
-import "./fonts/css/basic_4567.css";
+import font_path from "./fonts/css/basic_4567.css?url";
 import { SetRootBox } from "./components/SetShared";
 //import 'virtual:stylex.css';
 
@@ -12,6 +12,7 @@ const Root = (props: RouteSectionProps) => {
   return(
     <MetaProvider>
       <Title>PARTYCAL</Title>
+      <Link rel="stylesheet" href={font_path} />
       <Suspense>
         <SetRootBox>
           {props.children}
