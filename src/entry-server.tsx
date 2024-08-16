2dc6c4b4fffa10470c19a456282f24798aaac0b7
jaeeuu <jreset@naver.com>
2024-08-16 18:30:49 +0900

    fix

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,7 +9,7 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.svg" />
-          <link rel="stylesheet" href="/fonts/css/basic_5678.css" />
+          <link rel="stylesheet" href="/fonts/css/basic_4567.css" />
           {assets}
         </head>
         <body>
