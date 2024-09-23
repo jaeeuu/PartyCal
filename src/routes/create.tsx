8a8b3c0540d6ce245075ff9e72fa96de4f20bee1
jaeeuu <jreset@naver.com>
2024-09-23 20:16:24 +0900

    fix

--- src/routes/create.tsx
+++ src/routes/create.tsx
@@ -1,43 +1,41 @@
 import { SetBox } from "~/components/SetShared";
 import { useLocation } from "@solidjs/router";
-import { createResource, Suspense, Show, createMemo } from "solid-js";
-import * as stylex from "@stylexjs/stylex";
+import { createResource, Show } from "solid-js";
+// import * as stylex from "@stylexjs/stylex";
 import Spinner from "~/components/Spinner";
 
-type CreateStateProps = null | {
-  s?: number;
-  c?: number;
-  t?: string;
-  k?: boolean;
-};
+// type CreateStateProps = null | {
+//   s?: number;
+//   c?: number;
+//   t?: string;
+//   k?: boolean;
+// };
 
 export default function CreatePage() {
-  const location = useLocation();
-  const fetchLink = async (source: CreateStateProps) => {
-    try {
-      if (source.s && source.c && source.t){
-        const res = await fetch("https://partycal.site/apix/create", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(source),
-        });
-        if (res.ok) return res.json();
-      } throw new Error();
-    } catch {
-      return null;
-    }
-  };
+  
 
-  const [uid] = createResource(()=>location.state || {}, fetchLink);
+  
+  const [uid] = createResource(async () => {
+    const location = useLocation();
+    const res = await fetch("https://partycal.site/apix/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(location.state || {}),
+    });
+    if (res.ok) return res.json();
+    else return null;
+  });
 
 
   return (
     <SetBox>
-      <Suspense fallback={<Spinner />}>
-        <Show when={uid()}>{JSON.stringify(uid())}</Show>
-      </Suspense>
+      <Show
+        fallback={<Spinner />}
+        when={uid()}>
+          {JSON.stringify(uid())}
+      </Show>
     </SetBox>
   );
 }
